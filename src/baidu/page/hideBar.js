1a424339180f0835e59d6bf28e1ddec190f5e927
walter <shenhongshun@baidu.com>
2011-03-26 11:43:34 +0800

    hideBar 添加回调函数

--- src/baidu/page/hideBar.js
+++ src/baidu/page/hideBar.js
@@ -12,12 +12,19 @@
 
 /**
  * 隐藏地址栏
- *        注：页面高度应该大于等于可见高度才可隐藏地址栏
+ *  
+ * @param {Function}   fn 回调函数
+ * @remark  页面高度应该大于等于可见高度才可隐藏地址栏
  */
-baidu.page.hideBar = function () {
+baidu.page.hideBar = function (fn) {
     var hideFunc = function() {
             setTimeout(function() {
                 window.scrollTo(0, 1);
+                if(fn){
+                    setTimeout(function(){
+                        fn();
+                    }, 0)
+                }
             }, 0);
         }
     if (document.readyState == "complete") {
