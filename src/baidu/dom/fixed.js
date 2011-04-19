99d5e2b17643d79fc4b73ac1df37ae2c4e7def1d
walter <shenhongshun@baidu.com>
2011-04-19 20:38:59 +0800

    修改设置bottom不生效的bug

--- src/baidu/dom/fixed.js
+++ src/baidu/dom/fixed.js
@@ -1,63 +1,67 @@
 /*
- * Tangram Mobile
- * Copyright 2010 Baidu Inc. All rights reserved.
- * 
- * path: baidu/dom/fixed.js
- * author: bang
- * version: 1.0.0
- * date: 2010/12/6
- */
- 
-///import baidu.dom.g;
-///import baidu.page.getScrollTop;
-///import baidu.page.getScrollLeft;
-///import baidu.page.getWidth;
-///import baidu.page.getHeight;
+* Tangram Mobile
+* Copyright 2010 Baidu Inc. All rights reserved.
+*
+* path: baidu/dom/fixed.js
+* author: bang
+* version: 1.0.0
+* date: 2010/12/6
+*/
 
+///import baidu.dom.g;
+///import baidu.dom._styleFilter.px;
 ///import baidu.dom.setStyles;
 ///import baidu.event.customScroll;
 ///import baidu.event.on;
-///import baidu.browser.android;
+///import baidu.lang.isNumber;
 
 /**
  * 固定元素
  * @param {HTMLelem} elem  目标元素
- * @param {Object} options 选项  {top, left, right, bottom, fixedCompat(是否在桌面浏览器使用position:fixed)}
+ * @param {Object} options 选项  {top, left, right, bottom}
  */
 baidu.dom.fixed = function(elem, options) {
-    elem = baidu.g(elem);
+    elem = baidu.dom.g(elem);
     options = options || {};
     
-    if (options.fixedCompat && baidu.m.browser.android > 2.1) {
-        baidu.m.setStyles(elem, {
-            position: "fixed",
-            left: options.right ? "none" : options.left,
-            right: options.left ? "none" : options.right,
-            top: options.bottom ? "none" : options.top,
-            bottom: options.top ? "none" : options.bottom
-        });
-        return;
-    }
-    
-    baidu.m.setStyle(elem, "position", "absolute");
-    var setPos = function(){
-        var p = baidu.page;
-        baidu.m.setStyles(elem, {
-			//top,bottom left,right 同时出现时，以left,top为准
-            "top" : options.top ? p.getScrollTop() + (options.top || 0) : "none",
-            "left" : options.left ? p.getScrollLeft() + (options.left || 0) : "none",
-            "right" : options.left ? "none" : p.getWidth() - p.getScrollLeft() - window.innerWidth + (options.right || 0),
-            "bottom" : options.top ? "none" : p.getHeight() - p.getScrollTop() - window.innerHeight  + (options.bottom || 0)
-        });
-    }
-    setPos();
-    
-    baidu.m.event.on(document, "customScroll", function(e) {
-        if (e.scrollType == "scrollstop") {
-            setPos();
-            baidu.m.setStyle(elem,"display", "");
+    function setPos() {
+        var top = options.top,
+            bottom = options.bottom,
+            left = options.left,
+            right = options.right,
+            isNumber = baidu.lang.isNumber;
+
+        if(isNumber(top)) {
+            top += window.pageYOffset;
+        } else if(isNumber(bottom)) {
+            top = window.pageYOffset + window.innerHeight - elem.offsetHeight - bottom;
         } else {
-            baidu.m.setStyle(elem,"display", "none");
+            top = window.pageYOffset;
+        }
+        
+        if(isNumber(left)) {
+            left += window.pageXOffset;
+        } else if(isNumber(right)) {
+            left = window.pageXOffset + window.innerWidth - elem.offsetWidth - right;
+        } else {
+            left = window.pageXOffset;
+        }
+
+        baidu.dom.setStyles(elem, {
+            "top" : top,
+            "left" : left
+        });
+    }
+    
+    baidu.dom.setStyle(elem, "position", "absolute");
+    setPos();
+
+    baidu.event.on(document, "customScroll", function(e) {
+        if (e.scrollType == "scrollstop") {
+            baidu.dom.setStyle(elem,"display", "");
+            setPos();
+        } else {
+            baidu.dom.setStyle(elem,"display", "none");
         }
     }, true);
 };
