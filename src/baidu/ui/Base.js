8b43c206b37d6eed251af093d44dc4848c9df4f0
walter <shenhongshun@baidu.com>
2011-04-25 11:25:41 +0800

    修改base

--- src/baidu/ui/Base.js
+++ src/baidu/ui/Base.js
@@ -1,75 +1,62 @@
 /*
- * Tangram Mobile
- * Copyright 2011 Baidu Inc. All rights reserved.
- *
- * path: baidu/ui/Base.js
- * author: walter
- * version: 1.0.0
- * date: 2011/3/15
- */
+* Tangram Mobile
+* Copyright 2011 Baidu Inc. All rights reserved.
+*
+* path: baidu/ui/Base.js
+* author: walter
+* version: 1.0.0
+* date: 2011/3/15
+*/
 
-///import baidu.ui.Event;
+///import baidu.ui;
 ///import baidu.ui.getAttribute;
-///import baidu.ui.getParent;
 ///import baidu.ui.getAttributesByRole;
 ///import baidu.dom.g;
 
 baidu.ui.Base = {
-    
+
     /**
      * 渲染UI
      */
-    setup: function(){
+    setup: function() {
         var me = this;
         me._setup();
         me._init();
     },
-    
     /**
      * 读取页面配属项
      */
-    _setup: function(){
+    _setup: function() {
         var me = this,
             element = me.element = baidu.dom.g(me.element);
-        
-        me.parent = baidu.ui.getParent(element);
+
         me.roles = baidu.ui.getAttributesByRole(element);
         baidu.object.extend(me, baidu.ui.getAttribute(element));
-        
-        element.setAttribute('data-guid', me.guid); 
-        
+
+        element.setAttribute('data-guid', me.guid);
+
         me.dispatchEvent("onsetup");
     },
-    
-    /**
-     * 初始化页面
-     */
-    _init: function(){
-        var me = this;
-        me.dispatchEvent("onload");
-    },
-    
     /**
      * 获得class，支持skin
      *
      * @param {string} optional key
      * @return {string} className
      */
-    getClass: function(key){
+    getClass: function(key) {
         var me = this,
-            className = me.classPrefix,
-            skinName = me.skin;
-         if (key) {
-             className += '-' + key;
-             skinName += '-' + key;
-         }
-         if (me.skin) {
-             className += ' ' + skinName;
-         }
-         return className;
+        className = me.classPrefix,
+        skinName = me.skin;
+        if (key) {
+            className += '-' + key;
+            skinName += '-' + key;
+        }
+        if (me.skin) {
+            className += ' ' + skinName;
+        }
+        return className;
     },
-    
-    /** 
+    /**
      * 触发事件
      * @public
      * @param {String} eventName   要触发的事件名称
@@ -80,12 +67,13 @@ baidu.ui.Base = {
             e: e
         });
     },
-    
+
     /**
      * 销毁当前实例
      */
-    dispose: function(){
-        this.dispatchEvent("dispose");
-        baidu.lang.Class.prototype.dispose.call(this);
+    dispose: function() {
+        var me = this;
+        baidu.lang.Class.prototype.dispose.call(me);
+        me.dispatchEvent("dispose");
     }
 };
